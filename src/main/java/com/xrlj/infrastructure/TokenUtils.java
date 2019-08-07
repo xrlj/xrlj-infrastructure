f97a878cd06682df045761e69ba404ebda1a525c
xinxiamu <932852117@qq.com>
2019-08-07 23:30:10 +0800

    重构

---
+++ src/main/java/com/xrlj/infrastructure/TokenUtils.java
@@ -0,0 +1,43 @@
+package com.xrlj.infrastructure;
+
+import com.xrlj.utils.authenticate.JwtUtils;
+
+import java.util.Objects;
+
+public final class TokenUtils {
+
+    private static final String TOKEN_NOT_NULL_MSG = "token不能为null";
+
+    private TokenUtils() {
+    }
+
+    public static Long getUserId(String token) {
+        Objects.requireNonNull(token, TOKEN_NOT_NULL_MSG);
+        Long userId = JwtUtils.getPubClaimValue(token, Constants.JWT.JWT_CLAIM_KEY_USER_ID, Long.class);
+        return userId;
+    }
+
+    public static String getUsername(String token) {
+        Objects.requireNonNull(token, TOKEN_NOT_NULL_MSG);
+        String username = JwtUtils.getPubClaimValue(token, Constants.JWT.JWT_CLAIM_KEY_USERNAME, String.class);
+        return username;
+    }
+
+    public static int getUserType(String token) {
+        Objects.requireNonNull(token, TOKEN_NOT_NULL_MSG);
+        int userType = JwtUtils.getPubClaimValue(token, Constants.JWT.JWT_CLAIM_KEY_USER_TYPE, Integer.class);
+        return userType;
+    }
+
+    public static String getClientId(String token) {
+        Objects.requireNonNull(token, TOKEN_NOT_NULL_MSG);
+        String clientId = JwtUtils.getPubClaimValue(token, Constants.JWT.JWT_CLAIM_KEY_CLIENT_ID, String.class);
+        return clientId;
+    }
+
+    public static String getClientDeviceType(String token) {
+        Objects.requireNonNull(token, TOKEN_NOT_NULL_MSG);
+        String clientDeviceType = JwtUtils.getPubClaimValue(token, Constants.JWT.JWT_CLAIM_KEY_CLIENT_DEVICE_TYPE, String.class);
+        return clientDeviceType;
+    }
+}
