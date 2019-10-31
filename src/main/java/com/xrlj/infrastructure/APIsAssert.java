802634414108fdb161b608fd3fb5bcd95022b514
xinxiamu <932852117@qq.com>
2019-10-31 11:08:49 +0800

    重构

---
+++ src/main/java/com/xrlj/infrastructure/APIsAssert.java
@@ -0,0 +1,18 @@
+package com.xrlj.infrastructure;
+
+import com.xrlj.framework.spring.mvc.api.APIs;
+import com.xrlj.framework.spring.mvc.api.ApiException;
+
+/**
+ * 通用的统一api错误提示。
+ */
+public final class APIsAssert {
+
+    public static ApiException appNotFind() {
+        throw APIs.error(APIsErrorStatus.APP_NOT_FIND.status(),APIsErrorStatus.APP_NOT_FIND.msg(), null);
+    }
+
+    public static ApiException appNotFindOrCheck() {
+        throw APIs.error(APIsErrorStatus.APP_NOT_FIND_OR_CHECK.status(),APIsErrorStatus.APP_NOT_FIND_OR_CHECK.msg(), null);
+    }
+}
---
+++ src/main/java/com/xrlj/infrastructure/APIsErrorStatus.java
@@ -0,0 +1,53 @@
+package com.xrlj.infrastructure;
+
+import org.springframework.lang.Nullable;
+
+/**
+ * 全系统共用的业务提醒状态。
+ */
+public enum APIsErrorStatus {
+
+    APP_NOT_FIND(2000, "应用程序未注册"),
+    APP_NOT_CHECK(2001, "应用程序未审核通过"),
+    APP_NOT_FIND_OR_CHECK(2002, "应用程序未注册或未审核");
+
+    private final int status;
+    private final String msg;
+
+    APIsErrorStatus(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public int status() {
+        return status;
+    }
+
+    public String msg() {
+        return msg;
+    }
+
+    public static APIsErrorStatus valueOf(int statusCode) {
+        APIsErrorStatus apIsErrorStatus = resolve(statusCode);
+        if (apIsErrorStatus == null) {
+            throw new IllegalArgumentException("No matching constant for [" + statusCode + "]");
+        } else {
+            return apIsErrorStatus;
+        }
+    }
+
+    @Nullable
+    public static APIsErrorStatus resolve(int statusCode) {
+        APIsErrorStatus[] apIsErrorStatuses = values();
+        int size = apIsErrorStatuses.length;
+
+        for (int i = 0; i < size; i++) {
+            APIsErrorStatus apIsErrorStatus = apIsErrorStatuses[i];
+            if (apIsErrorStatus.status == statusCode) {
+                return apIsErrorStatus;
+            }
+        }
+
+        return null;
+    }
+}
